fcd6da9ea52043b57ced51deca07c3800f0d2d68
Konstantinos Voulgaridis <kos.voulgaridis@gmail.com>
2022-04-30 16:56:21 +0300

    Create Navbar (#2)

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,3 +1,23 @@
+import Styled from "./styles";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Styled.Container>
+      <Styled.Logo>Logo</Styled.Logo>
+      <Styled.IconsContainer>
+        <Styled.IconContainer>
+          <Styled.HeroBellIcon />
+          <Styled.Counter>2</Styled.Counter>
+        </Styled.IconContainer>
+        <Styled.IconContainer>
+          <Styled.HeroMailIcon />
+          <Styled.Counter>2</Styled.Counter>
+        </Styled.IconContainer>
+        <Styled.IconContainer>
+          <Styled.HeroCogIcon />
+          <Styled.Counter>2</Styled.Counter>
+        </Styled.IconContainer>
+      </Styled.IconsContainer>
+    </Styled.Container>
+  );
 }
