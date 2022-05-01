fb169b34f3fb5f6779e0f64a8738808ea53e4061
Konstantinos Voulgaridis <kos.voulgaridis@gmail.com>
2022-05-01 12:03:28 +0300

    Feature/card (#3)
    * Fix navbar spacing
    
    * Create Card layout
    
    * Implement like functionality

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,3 +1,35 @@
-export default function Card() {
-  return <div>Card</div>;
+import { useState } from "react";
+
+import Styled from "./styles";
+
+export default function Card({ post }) {
+  const [liked, setLiked] = useState(false);
+
+  if (!post) return null;
+
+  const HeartIcon = Styled[liked ? "HeroHeartFilledIcon" : "HeroHeartIcon"];
+
+  const handleHeartIconClick = () => setLiked((prevState) => !prevState);
+
+  return (
+    <Styled.Container>
+      <Styled.InfoContainer>
+        <Styled.UserImgContainer>
+          <Styled.UserImg layout="fill" src={post.userImg} />
+        </Styled.UserImgContainer>
+        <Styled.Fullname>{post.fullname}</Styled.Fullname>
+      </Styled.InfoContainer>
+
+      <Styled.PostImgContainer>
+        <Styled.PostImg layout="fill" src={post.postImg} />
+      </Styled.PostImgContainer>
+
+      <Styled.IconsContainer>
+        <HeartIcon onClick={handleHeartIconClick} />
+        <Styled.HeroChatIcon />
+        <Styled.HeroShareIcon />
+        <Styled.HeroInfoIcon />
+      </Styled.IconsContainer>
+    </Styled.Container>
+  );
 }
--- components/Card/styles.js
+++ components/Card/styles.js
@@ -1,1 +1,81 @@
-import styledComponents from "styled-components";
+import styled, { css } from "styled-components";
+import Image from "next/image";
+import {
+  ChatIcon,
+  HeartIcon,
+  InformationCircleIcon,
+  ShareIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartFilledIcon } from "@heroicons/react/solid";
+
+const Container = styled.div`
+  height: 280px;
+`;
+
+const Fullname = styled.span`
+  margin-left: 10px;
+`;
+
+const Icon = css`
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+  margin-right: 5px;
+`;
+
+const HeroChatIcon = styled(ChatIcon)(Icon);
+const HeroHeartIcon = styled(HeartIcon)(Icon);
+const HeroHeartFilledIcon = styled(HeartFilledIcon)(Icon);
+const HeroInfoIcon = styled(InformationCircleIcon)(Icon);
+const HeroShareIcon = styled(ShareIcon)(Icon);
+
+const IconsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+  padding-left: 3px;
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  font-weight: 500;
+  font-size: 14px;
+`;
+
+const PostImg = styled(Image)`
+  object-fit: cover !important;
+`;
+
+const PostImgContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 200px;
+`;
+
+const UserImgContainer = styled.div`
+  position: relative;
+  width: 30px;
+  height: 30px;
+`;
+
+const UserImg = styled(Image)`
+  border-radius: 50%;
+`;
+
+export default {
+  Container,
+  Fullname,
+  HeroChatIcon,
+  HeroHeartIcon,
+  HeroHeartFilledIcon,
+  HeroInfoIcon,
+  HeroShareIcon,
+  IconsContainer,
+  InfoContainer,
+  PostImg,
+  PostImgContainer,
+  UserImg,
+  UserImgContainer,
+};
